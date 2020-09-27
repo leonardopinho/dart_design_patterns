b1220bd74d837710e5436ae1bc14bcdc1171757b
Leonardo Pinho <leonardo.pinho@grupogaia.com.br>
2020-09-27 20:25:50 -0300

    added test for observer pattern

--- test/observerTest.dart
+++ test/observerTest.dart
@@ -5,21 +5,25 @@ enum ObserverType { INIT }
 
 void main() {
   test('should listen to observer event', () {
+    var listener = Observer.instance;
+    var listener2 = Observer.instance;
+    var result1, result2;
 
-    var observer = Observer.instance;
-    var result;
-
-    observer.registerObserver(ObserverType.INIT, (value){
-      if(value != null){
-        result = value;
-      }
+    listener.registerObserver(ObserverType.INIT, (value) {
+      result1 = value;
     });
 
-    observer.notifyObserver(ObserverType.INIT, value: 'message');
+    listener2.registerObserver(ObserverType.INIT, (value) {
+      result2 = value;
+    });
 
-    observer.removeObserver(ObserverType.INIT);
+    listener.notifyObserver(ObserverType.INIT, value: 'message');
 
-    expect(result, 'message');
+    listener.removeObserver(ObserverType.INIT);
 
+    listener2.removeObserver(ObserverType.INIT);
+
+    expect(result1, 'message');
+    expect(result1, result2);
   });
 }
